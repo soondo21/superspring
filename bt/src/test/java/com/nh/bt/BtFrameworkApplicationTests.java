7bd54cba4dda9008132d661c9e8b42679d00bb03
soondo_kwon <soondo_kwon@tmax.co.kr>
2022-02-11 11:05:22 +0900

    project first commit

---
+++ bt/src/test/java/com/nh/bt/BtFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nh.bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
